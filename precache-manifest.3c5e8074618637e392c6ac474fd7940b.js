1ec6ee1e03eec29d21cf52d0ffac2df04821c7c7
Arthur <arthur.bernier@blavity.com>
2020-10-12 13:53:39 -0400

    Updates

---
+++ precache-manifest.3c5e8074618637e392c6ac474fd7940b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cdbe85d50d09b0901191abd7db2b7c",
+    "url": "/ghpagestest/index.html"
+  },
+  {
+    "revision": "599ad2bd578e719c05aa",
+    "url": "/ghpagestest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c83d94f9fa6004d9e3e",
+    "url": "/ghpagestest/static/js/2.cd52aade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpagestest/static/js/2.cd52aade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599ad2bd578e719c05aa",
+    "url": "/ghpagestest/static/js/main.1306f9d5.chunk.js"
+  },
+  {
+    "revision": "3c53a99156b4b5350392",
+    "url": "/ghpagestest/static/js/runtime-main.7675b8c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpagestest/static/media/logo.5d5d9eef.svg"
+  }
+]);
